4cdc18623d2cbfc221a76617c2199b9311093bf7
Ojewale Opeoluwa <opeoluwa313@gmail.com>
2021-08-23 18:02:59 +0100

    Initial Commit

---
+++ store/tests/test_models.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from ..models import Category, Product
+
+
+class TestCategoriesModel(TestCase):
+
+    def setUp(self):
+        self.data1 = Category.objects.create(name='django', slug='django')
+
+    def test_category_model_entry(self):
+        data = self.data1
+        self.assertTrue(isinstance(data, Category))
+
+    def test_category_model_entry(self):
+        """
+        Test category model default name
+        """
+        data = self.data1
+        self.assertEqual(str(data), 'django')
+
+
+class TestProductsModel(TestCase):
+    def setUp(self):
+        Category.objects.create(name='django', slug='django')
+        User.objects.create(username='admin')
+        self.data1 = Product.objects.create(category_id=1, title='django stuffs'
+                                            , author='Rad', description='laokdnwWNInfenfnawj efuebgad gaeur',
+                                            slug='django-stuffs', price='20.45', created_by_id=1)
+
+    def test_products_model_entry(self):
+        data = self.data1
+        self.assertTrue(isinstance(data,Product))
+        self.assertEqual(str(data),'django stuffs')
